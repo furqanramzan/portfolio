8b403cbc4d3527f53fa30bcd66575d1b44689baf
Muhammad Furqan <furqanramzan271996@gmail.com>
2023-09-03 16:13:18 +0500

    feat(pwa): add manifest

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,29 @@ export default defineConfig({
       scope: '/',
       includeAssets: ['favicon.svg'],
       registerType: 'autoUpdate',
+      manifest: {
+        name: 'Muhammad Furqan',
+        short_name: 'Furqan Ramzan',
+        theme_color: '#92400E',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
       workbox: {
         globPatterns: ['**/*.{css,js,html,svg,png,webp,ico}'],
       },
