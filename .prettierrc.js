efe8ce8c293d3a03cc4eac845d2adb16d7c359cd
Muhammad Furqan <furqanramzan271996@gmail.com>
2023-09-08 15:43:41 +0500

    feat(prettier): add tailwind prettier plugin

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@
 export default {
   singleQuote: true,
   trailingComma: 'all',
-  plugins: ['prettier-plugin-astro'],
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
   overrides: [
     {
       files: '*.astro',
