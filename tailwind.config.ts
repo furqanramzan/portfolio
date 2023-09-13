225842fe5e3d57b6274c437f0b4c2900b2800b81
Muhammad Furqan <furqanramzan271996@gmail.com>
2023-09-13 06:41:20 +0500

    feat(aboutme): make about me section sticky

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,13 @@
 import type { Config } from 'tailwindcss';
 import flowbite from 'flowbite/plugin';
+import scrollbar from 'tailwind-scrollbar';
 
 const config: Config = {
   content: [
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
     './node_modules/flowbite/**/*.js',
   ],
-  plugins: [flowbite],
+  plugins: [flowbite, scrollbar],
   darkMode: 'class',
 };
 
